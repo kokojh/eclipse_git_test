2d47cbe7a491d498132fe5965026c1d55eb42998
ko <kjhzzang4692@naver.com>
2017-06-27 10:22:13 +0900

    master branch change a code

--- git_test/src/main/java/com/test/git/HomeController.java
+++ git_test/src/main/java/com/test/git/HomeController.java
@@ -28,7 +28,7 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		System.out.println("Hellow git");
-		
+		System.out.println("I am master");
 		return "home";
 	}
 	
